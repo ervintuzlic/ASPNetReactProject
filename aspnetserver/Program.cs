e0f4bc2fc33174ae8b015164e4513d8153508c64
Ervin Tuzlic <ervin@fragnet.net>
2022-08-24 10:10:33 +0200

    Swagger adjustments

--- aspnetserver/Program.cs
+++ aspnetserver/Program.cs
@@ -1,20 +1,29 @@
+using aspnetserver.Data;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(swaggerGenOptions =>
+{
+    swaggerGenOptions.SwaggerDoc("v1", new OpenApiInfo { Title = "ASP.NET React Project", Version = "v1" });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(swaggerUiOptions =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    swaggerUiOptions.DocumentTitle = "ASP.NET React Project";
+    swaggerUiOptions.SwaggerEndpoint("/swagger/v1/swagger.json", "Web API serving a very simple Post model.");
+    swaggerUiOptions.RoutePrefix = String.Empty;
+});
 
 app.UseHttpsRedirection();
 
+app.MapGet("/get-all-posts", async () => await PostsRepository.GetPostsAsync())
+    .WithTags("Posts Endpoints");
 
 app.Run();
