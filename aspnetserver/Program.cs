2b70490a3b1ca33bb01ac798d69a059fbd1147c5
Ervin Tuzlic <ervin@fragnet.net>
2022-08-24 11:31:09 +0200

    API Endpoints

--- aspnetserver/Program.cs
+++ aspnetserver/Program.cs
@@ -26,4 +26,44 @@ app.UseHttpsRedirection();
 app.MapGet("/get-all-posts", async () => await PostsRepository.GetPostsAsync())
     .WithTags("Posts Endpoints");
 
+app.MapGet("/get-post-by-id/{postId}", async (int postId) =>
+ {
+     Post postToReturn = await PostsRepository.GetPostByIdAsync(postId);
+
+     if (postToReturn != null)
+         return Results.Ok(postToReturn);
+     else
+         return Results.BadRequest();
+ }).WithTags("Posts Endpoints");
+
+
+app.MapPost("/create-post", async (Post postToCreate) =>
+{
+    bool createPost = await PostsRepository.createPostAsync(postToCreate);
+
+    if (createPost)
+        return Results.Ok("Create successful.");
+    else
+        return Results.BadRequest();
+}).WithTags("Posts Endpoints");
+
+
+app.MapPut("/update-post", async (Post postToUpdate) =>
+{
+    bool updatePost = await PostsRepository.updatePostAsync(postToUpdate);
+    if (updatePost)
+        return Results.Ok("Post updated successfully");
+    else
+        return Results.BadRequest();
+}).WithTags("Posts Endpoints");
+
+app.MapDelete("/delete-post-by-id/{postId}", async (int postId) =>
+{
+    bool postToDelete = await PostsRepository.deletePostAsync(postId);
+    if (postToDelete)
+        return Results.Ok("Post deleted successfully");
+    else
+        return Results.BadRequest();
+}).WithTags("Posts Endpoints");
+
 app.Run();
